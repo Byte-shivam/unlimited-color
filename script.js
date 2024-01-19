41f36e6264196e2e2e26d25674b53f5ee8058562
shivamcoder73 <123951143+shivamcoder73@users.noreply.github.com>
2024-01-19 17:16:50 +0530

    new project added

---
+++ script.js
@@ -0,0 +1,26 @@
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color= '#';
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+let intervalId;
+const startChangingColor = function(){
+    if(!intervalId){
+        intervalId= setInterval(changeBGcolor,1000);
+    }
+    
+    function changeBGcolor(){
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+document.querySelector('#start').addEventListener('click',startChangingColor) 
+        
+const stopChangingColor = function(){
+   clearInterval(intervalId);
+   intervalId= null;
+}
+document.querySelector('#stop').addEventListener('click',stopChangingColor) 
+        
